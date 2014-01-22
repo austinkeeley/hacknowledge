d495df43e9ef820fff86383fff60803aa50fa155
Austin Keeley <austin.keeley@gmail.com>
2014-01-22 09:18:33 -0500

    Updating .gitignore for vim fils and adding pages controller

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,10 @@
+# pages_controller.rb
+# A controller for doing basic static pages.  Try not to
+# put too much in this controller and opt for a more MVC 
+# pattern with partials instead.
+class PagesController < ApplicationController
+
+  def index
+  end
+
+end
