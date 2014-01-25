448e5079b04d8498ce2b019b4f377c418a704019
Austin Keeley <austin.keeley@gmail.com>
2014-01-24 20:56:22 -0500

    Before I potentially destroy my db

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,11 +1,20 @@
 class OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
   def all
-    raise request.env['omniauth.auth'].to_yaml
 
+    u = User.from_omniauth(request.env['omniauth.auth'])
+    
+    if u
+      sign_in u
+    else
+      raise request.env['omniauth.auth'].to_yaml
+
+    end
     # Here you should find authentication object that matcehs the omniauth.auth
     # details and then return that user.  This won't work with additional
     # authentication methods though... you'll have to figure that out. 
+    
+
   end
 
   alias_method :twitter, :all
--- app/models/user.rb
+++ app/models/user.rb
@@ -15,4 +15,31 @@ class User < ActiveRecord::Base
   # We want to provide many ways for a user to login through
   # omniauth (Twiter, Facebook, etc.)
   has_many :authentications
+
+  # People might be using other authentication methods, so we'll remove 
+  # the need for email
+  def email_required?
+    false
+  end
+  def email_changed?
+    false
+  end
+
+
+  # Using an auth object captured from an omniauth callback,
+  # find the user in the database or create a new user and
+  # add this as an authentication method.
+  def self.from_omniauth(auth)
+    uid = auth.uid
+    provider = auth.provider
+
+    authentication = Authentication.find_or_create_by(:provider => provider, :uid => uid)
+    if !authentication.user
+      user = User.new
+      authentication.user = user
+      user.save
+    end
+    return authentication.user
+  end
+
 end
