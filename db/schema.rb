53a8ca638007a52e7214bd5b20d7922a5163e1ba
Austin Keeley <austin.keeley@gmail.com>
2014-01-29 21:42:40 -0500

    Adding schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140130012727) do
+ActiveRecord::Schema.define(version: 20140130013427) do
 
   create_table "authentications", force: true do |t|
     t.integer  "user_id"
@@ -92,6 +92,7 @@ ActiveRecord::Schema.define(version: 20140130012727) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "event_id"
+    t.text     "description"
   end
 
   create_table "users", force: true do |t|
